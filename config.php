c2b24b39424c3ff158dec6b1bc19c08b0bb86012
Vigraman Padmanaban <pvigraman@gmail.com>
2018-10-19 15:47:14 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
 	'database' => [
 		'name' => 'horsesimdb',
 		'username' => 'root',
-		'password' => '082Aug86',
+		'password' => '',
 		'connection' => 'mysql:host=127.0.0.1',
 		'options' => [
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
